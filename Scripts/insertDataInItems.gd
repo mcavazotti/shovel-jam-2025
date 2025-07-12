2fd816786dc16c0a766457e72484edfebe9f1c9a
Renato Real <90974187+RRealJ@users.noreply.github.com>
2025-07-12 16:20:10 -0300

    random items connection
    Added Atributes to item class.
    
    Added a way to connect the ItemObjects in scene with object data for random generation.

---
+++ Scripts/insertDataInItems.gd
@@ -0,0 +1,36 @@
+extends Node2D
+
+
+@onready var children = $".".get_children()
+var copiedItemData = ItemDataLoad.getCopiedData()
+var roullete = []
+
+func _ready():
+	randomize()
+	copiedItemData = ItemDataLoad.getCopiedData()
+	for item in copiedItemData:
+		roullete.append(item["id"])
+		
+	for child in children:
+		if child is Item:
+			connectItemData(child)
+			
+	#print(ItemDataLoad.itemData) Verify if ItemData is NOT modified
+			
+
+func connectItemData(itemInHouse): #Put randomItem data inside Item
+	var randomIndex = randi() % roullete.size()
+	var randomId = roullete[randomIndex]
+	for item in copiedItemData:
+		if item["id"] == randomId:
+			itemInHouse.id = item["id"]
+			itemInHouse.name = item["name"]
+			itemInHouse.tags = item["tags"]
+			itemInHouse.category = item["category"]
+			itemInHouse.description = item["description"]
+			itemInHouse.image = item["image"]
+			itemInHouse.shape = item["shape"]
+			
+			copiedItemData.erase(item)
+
+#print(copiedItemData) Verify if CopiedItemData is erasing properly
