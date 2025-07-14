c4239cb93a0460f9b883df4fb546c0a3df09f18a
Renato Real <90974187+RRealJ@users.noreply.github.com>
2025-07-14 16:42:14 -0300

    Endings JSON and Final Trigger
    -added way to verify which ending is available.
    -added way to prevent multiples of the "same" ending
    -added 2 new fields on Endings JSON
       -reference : "string"
       -typeOfEnding: "string"

--- Scripts/finalEndingVerify.gd
+++ Scripts/finalEndingVerify.gd
@@ -7,11 +7,13 @@ var endingId : int
 var endingName : String
 var endingStory : String
 var endingCoverImage : String
-var possibleEndings : Array[String] = []
+var possibleEndings = [] #{id, typeOfEnding}
+var intermediateEndings = [] #[{id:id, story:story},{id:id, story:story}]
 var bag = []
 
 
 func _ready() -> void:
+	clearEndings()
 	bag = [
 		{	
 			"itemId" : 3001,
@@ -62,55 +64,103 @@ func verifyEnding(bag):
 					itemCategories.append(items[key])
 					
 				"itemTags":
-					for i in items[key]:
+					for i in items[key]: #i being the items in the Tags array.
 						itemTags.append(i)
 						
 	matchEnding(itemIds, itemCategories, itemTags)
+	preventConflictEnding()
+	finalEndingSelect()
 
 	
 func matchEnding(ids, categories, tags):
-	print(ids)
-	print(categories)
-	print(tags)
 	var endings = endingList
 	for ending in endings: #loop through the endings
 		var isEndingOk : Array[bool] = []
-		print()
-		print(ending)
-		print()
 		
 		#var true_id = str(int(ending["id"])).pad_zeros(4) #USE THIS, BUT THIS IS A STRING
-		#print(true_id)
 		
 		for conditions in ending["conditions"]:
-			var count = 0
-			print(conditions)
-
-			if conditions["type"] == "category":
-				if int(conditions["value"]) in categories: #needs to be int
-					count = categories.count(int(conditions["value"])) #needs to be int
-					var result = matchOperator(count, conditions["countType"], conditions["count"])
-					
-					if result == conditions["shouldBeTrue"]:
-						isEndingOk.append(true)
-					else:
-						isEndingOk.append(false)
+			var count = 0	
+			var c = conditions["type"]
+			var value = conditions["value"]
+			
+			match c: 
+				"category":
+					if int(value) in categories: 
+						count = categories.count(int(value))
 						
-		for verifyTrue in isEndingOk:
-			if verifyTrue:
-				possibleEndings.append(ending["id"])
-
+				"id":	
+					if int(value) in ids: 
+						count = ids.count(int(value))
+						
+				"tag":	
+					if value in tags: 
+						count = tags.count(value)
+									
+			var result = matchOperator(count, conditions["countType"], conditions["count"])
+			if result == conditions["shouldBeTrue"]: 
+				isEndingOk.append(true)
+			else:
+				isEndingOk.append(false)	
+											
+		if not isEndingOk.has(false): 
+			possibleEndings.append({"id": ending["id"], "typeOfEnding": ending["typeOfEnding"], "reference": ending["reference"]})
+			#edit dicionary to match keys.
  
-func matchOperator(count, type, condition):
-	pass
 
+func matchOperator(count, type, condition):
+	match type:
+		"==": 
+			return count == condition
+		">=":
+			return count >= condition
+		"!=":
+			return count != condition
+		"<=":
+			return count <= condition
+		">":
+			return count > condition
+		"<":
+			return count > condition
+		
+		
 func launchCutscene():
 	pass
 
 
 func _on_button_down() -> void: #button for testing, exclude afterwards
 	var endingFinal = verifyEnding(bag)
-	#endingId = endingFinal["endingId"]
-	#endingName = endingFinal["endingName"]
-	#endingStory = endingFinal["endingStory"]
-	#launchCutscene()
+
+
+func preventConflictEnding():
+	var sameReference = []
+	var finalEndingVerify = []
+	var index: int
+	
+	for ending in possibleEndings:
+		if ending["typeOfEnding"] == "final":
+			index = sameReference.find(ending["reference"])
+			
+			if index != -1:
+				sameReference.remove_at(index)
+				finalEndingVerify.remove_at(index)
+				
+			sameReference.append(ending["reference"])
+			finalEndingVerify.append(ending["id"])
+			
+			print(finalEndingVerify)
+			print(sameReference)
+
+		
+func finalEndingSelect():
+		
+
+func clearEndings():
+	endingId = 0
+	endingName = ""
+	endingStory = ""
+	endingCoverImage = ""
+	possibleEndings = [] 
+	intermediateEndings = [] 
+		
+		
