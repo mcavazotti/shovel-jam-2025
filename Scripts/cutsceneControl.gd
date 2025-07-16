62564b770e9d93dfdbbba3a5b22007062afd034b
Renato Real <90974187+RRealJ@users.noreply.github.com>
2025-07-15 22:48:58 -0300

    -added cutscene control
    show the final ending with background image cover and the intermediate edings.
    
    will add a way to stich the intermediate endings together

--- Scripts/cutsceneControl.gd
+++ Scripts/cutsceneControl.gd
@@ -1,15 +1,15 @@
-extends Node2D
+extends Control
 
 
 @onready var anim = $anim
-var tween := create_tween()
-
+@onready var image = $TextureRect
 
 func _ready() -> void:
-	pass
+	var screen_size = DisplayServer.screen_get_size()
+	anim.play("IntermediateEndings")	
+	image.custom_minimum_size = Vector2(screen_size.x - 200, screen_size.y - 112.5)
 	
 
-func resetTween() -> void:
-	if tween:
-		tween.kill()
-	tween = create_tween()
+func _on_anim_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "IntermediateEndings":
+		anim.play("FinalEnding")
