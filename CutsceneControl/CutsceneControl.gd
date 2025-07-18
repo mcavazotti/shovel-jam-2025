01904e64f98298a8a2eee9fa38065946a9675937
Renato Real <90974187+RRealJ@users.noreply.github.com>
2025-07-18 16:00:53 -0300

    Improved Cutscene Final and Shader
    -added Intermediate images in cutscene
    -added shader to simulate memory eff in images
    -reorganizing AnimPlayer to better visualize and time methods.

--- CutsceneControl/CutsceneControl.gd
+++ CutsceneControl/CutsceneControl.gd
@@ -2,6 +2,7 @@ extends Control
 
 
 @onready var anim = $anim
+@onready var animImage = $animImage
 @onready var image = $finalImage
 @onready var labelIntermediate = $intermediate 
 @onready var labelFinalTitle = $finalTitle
@@ -16,11 +17,17 @@ var finalEnding
 var speed_up = false
 var paused = false
 var speedOriginal : int
+var indexImages: int = 0
+var interImages:Array 
+var textureRects:Array
 
 
 func _ready() -> void:
 	endIds.clear()
 	image.custom_minimum_size = DisplayServer.screen_get_size()
+	textureRects = get_tree().get_nodes_in_group("InterImages")
+	print("Texture Rects: ", textureRects)
+	speedOriginal = 1.0
 	
 
 func finalAnimStart() -> void:
@@ -40,8 +47,11 @@ func treatEnding():
 		for ending in intermediateEndings:
 			stichedIntermediateEndings += "%s\n\n" % ending["story"]
 			endIds.append(ending["id"])
+		
 			$son.text = "THE GREAT LIFE OF YOUR SON"
 			endingsCount += 1
+			
+			interImages.append(ending["coverImage"])
 			
 	elif (not intermediateEndings) and FinalEndingLoad.itemsBackpack: #This will also trigger if there's no intermidiate ending
 		stichedIntermediateEndings +=  "You know too well the dangers of an adventure. But you decide to let your son life his adventures anyway.
@@ -49,9 +59,12 @@ func treatEnding():
 From time to time you heard a few news about him...”"
 		
 	elif not intermediateEndings:
+		interImages.append("")
 		stichedIntermediateEndings += "You know too well the dangers of an adventure. You’re not letting your child be in harm’s way, no matter what!
 
 ”Mom! Why you never let me do anything?!”"
+
+		
 		
 		
 	endIds.append(finalEnding["id"])
@@ -68,11 +81,27 @@ From time to time you heard a few news about him...”"
 	
 	labelEndingCount.text = "%s/%s" % [str(sizeUnlocked), str(total)]
 	labelEndingLocked.text = "There's still %s endings locked" % [total - sizeUnlocked]
-	print(SaveNLoad.endingUnlocked)
+
 	SaveNLoad.save_game()
 	skipIntermediateUpdate(endingsCount)
+	
+	updateIntermediateImages() #FIRST RUN IN SCENE. (IMPORTANT BE HERE)
 	startAnimation()
 	
+	
+func updateIntermediateImages():
+	print("InterImages: ", interImages)
+	for texRect in textureRects:
+		if indexImages < interImages.size():
+			print("Texture Rect", texRect)
+			#texRect.texture = interImages[indexImages]
+			print("TEXTURE RECT CHANGED IMAGE OK")
+			indexImages += 1
+
+
+func playImages():
+	animImage.play("ImagesFadeInOut")
+		
 
 func startAnimation():
 	$buttons/buttonMenu.visible = false
@@ -117,16 +146,20 @@ func _unhandled_key_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_text_submit"): # ENTER
 		speed_up = !speed_up
 		if not speed_up and not paused:
+			
 			anim.speed_scale = 1
+			animImage.speed_scale = 1
 			$GPUParticles2D_Right.speed_scale = 1
 			$GPUParticles2D_Left.speed_scale = 1
 			speedOriginal = $GPUParticles2D_Right.speed_scale
+		#dont place speedOriginal out of If, might mess with Particles
 		elif speed_up and not paused:
+			
 			anim.speed_scale = 3
+			animImage.speed_scale = 3
 			$GPUParticles2D_Right.speed_scale = 3
 			$GPUParticles2D_Left.speed_scale = 3
 			speedOriginal = $GPUParticles2D_Right.speed_scale	
-				
 		#dont place speedOriginal out of If, might mess with Particles
 	
 	elif event.is_action_pressed("ui_select"):
@@ -137,9 +170,10 @@ func _unhandled_key_input(event: InputEvent) -> void:
 		else:
 			$GPUParticles2D_Left.speed_scale = speedOriginal
 			$GPUParticles2D_Right.speed_scale = speedOriginal
+			
 		anim.playback_active = !anim.playback_active
+		animImage.playback_active = !animImage.playback_active
 		
-
 func _on_button_menu_button_down() -> void:
 	Audio.Play(Audio.TRACK_ALIAS.Click)
 	get_tree().change_scene_to_file("res://Scenes/Title/title.tscn")
@@ -148,7 +182,7 @@ func _on_button_house_button_down() -> void:
 	Audio.Play(Audio.TRACK_ALIAS.Click)
 	get_tree().change_scene_to_file("res://Scenes/Home/home.tscn")
 
-
 func _on_button_replay_button_down() -> void:
+	indexImages = 0
 	Audio.Play(Audio.TRACK_ALIAS.Click)
 	startAnimation()
