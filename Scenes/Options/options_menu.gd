94b33a8f4dac8bb997d64dcb7d04ae59e7130661
Noah <Weedenenoah@gmail.com>
2025-07-19 11:17:01 -0400

    Updated Acheivements Scene

--- Scenes/Options/options_menu.gd
+++ Scenes/Options/options_menu.gd
@@ -6,10 +6,6 @@ func _on_return_pressed():
 	visible = false
 
 
-func _on_exit_game_pressed():
-	get_tree().change_scene_to_file("res://Scenes/Title/title.tscn")
-
-
 func _on_fullscreen_pressed():
 	Audio.Play(Audio.TRACK_ALIAS.Click)
 	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
@@ -41,3 +37,7 @@ func _on_sfx_slider_drag_ended(_SFX_value_changed: bool):
 
 func _on_amb_slider_drag_ended(_AMB_value_changed: bool):
 	Audio.AMB_volume_set($"TabContainer/Audio/Buttons and Title/AMB Slider".value)
+
+
+func _on_to_title_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Title/title.tscn")
--- Scenes/achievements.gd
+++ Scenes/achievements.gd
@@ -4,3 +4,7 @@ func _ready():
 	Audio.Play(Audio.TRACK_ALIAS.Results)
 #	for item in endings_gotten_list:
 #	show image and the text on the next line :P
+
+
+func _on_to_title_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Title/title.tscn")
