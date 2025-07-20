8f2a7b867a429d844a9946979e0a5a4b0e91f312
Noah <Weedenenoah@gmail.com>
2025-07-20 10:57:26 -0400

    Changed Bag Appearence

--- Backpack/BackpackController.cs
+++ Backpack/BackpackController.cs
@@ -11,12 +11,16 @@ public partial class BackpackController : CanvasLayer
 	public delegate void ContentChangeEventHandler(ItemData[] items);
 
 	private Backpack _backpack;
+	private Control _openArea;
+	private Control _closeArea;
 
 	public Backpack Backpack { get => _backpack; }
 
 	public override void _Ready()
 	{
 		_backpack = GetNode<Backpack>("Backpack");
+		_openArea = GetNode<Control>("UiArea/OpenArea");
+		_closeArea = GetNode<Control>("UiArea/CloseArea");
 	}
 
 
